9ebd3bc5e0e27a9f017b4497a92a4fb675d55187
psyomn <lethaljellybean@gmail.com>
2014-08-24 14:54:03 -0400

    src/message_handler.c: add error sendbacks

--- src/message_handler.c
+++ src/message_handler.c
@@ -9,13 +9,14 @@ psy_lbm_handle_message(psy_lbm_server_t* _s, remote_host_t* _h,
                        char* _message) {
   char delimiters[] = "|";
   char *token;
+  int badrequest = 0;
 
   /* First token */
   token = strtok(_message, delimiters);
 
   if (token == NULL) {
     /* Someone sent something empty in the first token (eg: "|||") */
-    return;
+    token = "badrequest";
   }
 
   if (!strcmp(token, "auth")) {
@@ -33,12 +34,19 @@ psy_lbm_handle_message(psy_lbm_server_t* _s, remote_host_t* _h,
     char* user = strtok(NULL, delimiters);
     char* pass = strtok(NULL, delimiters);
     printf("Registration request [%s]\n", user);
-
-    psy_lbm_handle_register(_s, _h, user, pass);
+    
+    if (user != NULL && pass != NULL)
+      psy_lbm_handle_register(_s, _h, user, pass);
+    else {
+      badrequest = 1;
+    }
   }
 
-  else {
-    printf("received weird command\n");
+  if (badrequest) {
+    /* TODO: a logger would be nice here */
+    printf("Received bad request [%s]\n", _message);
+    sendto(_s->sock, PSYLBM_BAD_REQUEST, sizeof(PSYLBM_BAD_REQUEST), 
+           0, (struct sockaddr*)&_h->a, _h->l);
   }
 
 }
@@ -81,17 +89,17 @@ psy_lbm_handle_register(psy_lbm_server_t* _s, remote_host_t* _h, char* _user,
     /* User already exist */
     sendto(_s->sock, PSYLBM_USERNAME_TAKEN, sizeof(PSYLBM_USERNAME_TAKEN), 
            0, (struct sockaddr*)&_h->a, _h->l);
-    printf("--| Username [%s] has been taken.\n", _user);
+    printf("--| Username [%s] has been taken.\n", _user); /* TODO: logme */
   }
   else {
     /* Everything ok */
     sendto(_s->sock, PSYLBM_REGISTRATION_OK, sizeof(PSYLBM_REGISTRATION_OK), 
            0, (struct sockaddr*)&_h->a, _h->l);
-    printf("--| Username [%s] registered.\n", _user);
+    printf("--| Username [%s] registered.\n", _user); /* TODO: logme */
   }
 }
 
-/* In case we ever want to log, this should handle the error */
+/* TODO In case we ever want to log, this should handle the error */
 int
 psy_lbm_handle_error(char* _sent_stuff) {
 
