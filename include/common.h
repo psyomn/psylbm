e12965e5c6e3c7e99d492f0b268f54c3169db821
psyomn <lethaljellybean@gmail.com>
2020-03-21 22:09:42 -0400

    Add common string functionality
    This should be nuked from orbit once we got better string support.

--- include/common.h
+++ include/common.h
@@ -1,5 +1,7 @@
 #pragma once
 
+// TODO: get rid of this file
+
 #include <stdint.h>
 
 #include <sys/types.h>
@@ -14,3 +16,9 @@
 #include <time.h>
 
 #include <openssl/sha.h>
+
+// TODO: this is here because sqlite3 returns (rightfully) const
+// unsigned char* strings from the database, because UTF-8. I should
+// revisit the structures handling strings, and fix them. That will
+// have to be yet another maintenance pass.
+char* psylbm_strndup(const unsigned char* src, size_t len);
---
+++ src/common.c
@@ -0,0 +1,17 @@
+#include "common.h"
+
+char* psylbm_strndup(const unsigned char* src, size_t len)
+{
+        // sqlite3 does not store length for the 0 termination of
+        // strings.
+        const size_t c_str_len = len + 1;
+
+        char* ret = calloc(0, sizeof(char) * c_str_len);
+
+        // TODO: this is bad because UTF. FIXME
+        memcpy(ret, src, len);
+
+        ret[len] = 0;
+
+        return ret;
+}
